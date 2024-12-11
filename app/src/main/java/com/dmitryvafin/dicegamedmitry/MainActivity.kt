8c0b1c92038b7b79ad0bda0c4d8610ac73b3bd7f
Dmitry <dmivaf@alu.edu.gva.es>
2024-12-11 11:21:28 +0100

    hechos botones DiceGameDmitry

--- app/src/main/java/com/dmitryvafin/dicegamedmitry/MainActivity.kt
+++ app/src/main/java/com/dmitryvafin/dicegamedmitry/MainActivity.kt
@@ -4,15 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dmitryvafin.dicegamedmitry.model.Dice
 import com.dmitryvafin.dicegamedmitry.ui.theme.DiceGameDmitryTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,20 +45,71 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Content(modifier: Modifier = Modifier) {
-
+    var puntos by rememberSaveable { mutableIntStateOf(20) }
+    var tiradas by rememberSaveable{ mutableIntStateOf(0)}
+    var num by rememberSaveable{ mutableIntStateOf(Dice.throwDiceNFaces(6))}
     Column(
-        modifier = Modifier.fillMaxSize().then(modifier)
-    ){
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Row {
-            Text(
-                text = "Hello toilet!", modifier = modifier
-            )
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "Juego de dados", fontSize = 25.sp)
+                Text(text = "-Las flechas son para apostar que saldrá un número mayor(arriba) o menor(abajo) que el dado.", fontSize = 15.sp)
+                Text(text = "-Los números son para apostar que saldrá ese número en el dado.", fontSize = 15.sp)
+                Text(text = "Flechas: paga 1 y gana 3", fontSize = 15.sp)
+                Text(text = "Números: paga 3 y gana 15", fontSize = 15.sp)
+            }
         }
+        Row{
+            Column(horizontalAlignment = Alignment.CenterHorizontally){
+                Button(onClick = { num = Dice.throwDiceNFaces(6)}, modifier = Modifier.size(100.dp), shape = RoundedCornerShape(10.dp)){
+                    Text(text = "$num", fontSize = 50.sp)
+                }
+            }
+            Column(modifier = Modifier.padding(start = 30.dp),horizontalAlignment = Alignment.CenterHorizontally){
+                Text(text = "Puntos:", fontSize=15.sp)
+                Text(text = "$puntos", fontSize = 50.sp)
+            }
+        }
+        Row{
+            Column { Button(onClick = { num = Dice.throwDiceNFaces(6)}, modifier = Modifier.size(50.dp), shape = RoundedCornerShape(10.dp)){
+                Text(text = "1", fontSize = 25.sp)
+            }
+                Button(onClick = { num = Dice.throwDiceNFaces(6)}, modifier = Modifier.size(50.dp), shape = RoundedCornerShape(10.dp)){
+                    Text(text = "2", fontSize = 25.sp)
+                }
+                Button(onClick = { num = Dice.throwDiceNFaces(6)}, modifier = Modifier.size(50.dp), shape = RoundedCornerShape(10.dp)){
+                    Text(text = "3", fontSize = 25.sp)
+                }
+            }
+            Column { Button(onClick = { num = Dice.throwDiceNFaces(6)}, modifier = Modifier.size(50.dp), shape = RoundedCornerShape(10.dp)){
+                Text(text = "4", fontSize = 25.sp)
+            }
+                Button(onClick = { num = Dice.throwDiceNFaces(6)}, modifier = Modifier.size(50.dp), shape = RoundedCornerShape(10.dp)){
+                    Text(text = "5", fontSize = 25.sp)
+                }
+                Button(onClick = { num = Dice.throwDiceNFaces(6)}, modifier = Modifier.size(50.dp), shape = RoundedCornerShape(10.dp)){
+                    Text(text = "6", fontSize = 25.sp)
+                }
+            }
+            Column { Button(onClick = { num = Dice.throwDiceNFaces(6)}, modifier = Modifier.size(50.dp), shape = RoundedCornerShape(10.dp)){
+                Text(text = "⬆", fontSize = 25.sp)
+            }
 
+                Button(onClick = { num = Dice.throwDiceNFaces(6)
+                                 puntos--}, modifier = Modifier.size(50.dp), shape = RoundedCornerShape(10.dp)){
+                    Text(text = "⬇", fontSize = 25.sp)
+                }
+            }
+        }
+        Row(modifier = modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceEvenly, verticalAlignment = Alignment.Bottom){
+            Text(text = "Dmitry Vafin")
+        }
     }
-
-
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
